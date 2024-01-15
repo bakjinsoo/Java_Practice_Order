5293582835cac1156c968a702e7f181d6e1d2d30
bakjinsoo <77185565+bakjinsoo@users.noreply.github.com>
2024-01-15 11:08:49 +0900

    스프링컨테이너 스프링 빈 0115

--- core/src/main/java/hello/core/order/AppConfig.java
+++ core/src/main/java/hello/core/order/AppConfig.java
@@ -6,17 +6,24 @@ import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService() {//생성자 주입
         return new MemberServiceImpl(memberRepository());
     }
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
+    @Bean
     public OrderService orderService() {//생성한 객체 인스턴스의 참조(레퍼런스)를 생성자를 통해서 주입(연결)해준다.
         return new OrderServiceImpl(memberRepository(),discountPolicy());
     }
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new FixDiscountPolicy();
     }
