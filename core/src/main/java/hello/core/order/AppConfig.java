8a7d2feec5ba6f979d725dc43f37ea6ec259e131
bakjinsoo <77185565+bakjinsoo@users.noreply.github.com>
2024-01-16 18:41:15 +0900

    @Configuration과 싱글톤/

--- core/src/main/java/hello/core/order/AppConfig.java
+++ core/src/main/java/hello/core/order/AppConfig.java
@@ -11,16 +11,21 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+    //@Bean memberService -> new MemoryMemberRepsitory()
+    //@Bean orderService -> new MemoryMemberRepositiory()
     @Bean
     public MemberService memberService() {//생성자 주입
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
     @Bean
     public OrderService orderService() {//생성한 객체 인스턴스의 참조(레퍼런스)를 생성자를 통해서 주입(연결)해준다.
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(),discountPolicy());
     }
     @Bean
