afefcd4fee39d0f9fd07cfdb7516d481cb980f46
PARK JIN SOO <77185565+bakjinsoo@users.noreply.github.com>
2024-01-16 23:30:35 +0900

    컴포넌트스캔/탐색위치/기본스캔대상/필터공부

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -10,5 +10,4 @@ import org.springframework.context.annotation.FilterType;
 )
 //Configuration에는 Component어노테이션이 존재하기 때문에,ComponentScan에서 검색됨 AppConfig.class를 안전하게 유지하기 위해 Configuration이 붙어 있는 클래스를 필터링해서 제외 시킨것.
 public class AutoAppConfig {
-
 }
